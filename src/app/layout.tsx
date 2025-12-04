70e0ea80713f0a1cb6409b0304b7d4ff77d0f0ee
Tomasz Zając <u56_tomzaj_lbn@technischools.com>
2025-12-04 14:12:35 +0100

    chore: init and setup firebase, small adjustments to header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,26 @@
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+import type { Metadata } from "next";
+import "./globals.css";
+import { Header } from "@/shared/Header";
+
+export const metadata: Metadata = {
+  title: "Spendly",
+  description:
+    "Spendly is the modern finance application that helps you track expenses, manage budgets, and save money effortlessly. Take control of your financial future today.",
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
-    <html lang="en">
-      <body>
-        {children}
+    <html lang="pl">
+      <body className="flex flex-col min-h-screen">
+        <Header />
+        <main className="flex-grow">{children}</main>
       </body>
     </html>
   );
