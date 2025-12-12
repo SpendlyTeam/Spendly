6e61f9b79bfab2dbe1a1be25ff48b14227ef498d
Patryk <patryk52310@gmail.com>
2025-12-12 14:09:16 +0100

    chore: dashboard init

---
+++ src/features/dashboard/ui/charts/monthly-line-chart.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import "@/shared/lib/chartjs";
+import { Line } from "react-chartjs-2";
+
+export function MonthlyLineChart(props: {
+  labels: string[];
+  values: number[];
+}) {
+  const data = {
+    labels: props.labels,
+    datasets: [{ data: props.values }],
+  };
+
+  return <Line data={data} />;
+}
